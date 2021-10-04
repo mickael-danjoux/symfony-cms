51f1b0588d285fce8d620c12d4b8905fa3c90761
Mickael <mickael@ab6net.net>
2021-10-04 21:46:19 +0200

    Cookies manager

--- assets/js/front/app.js
+++ assets/js/front/app.js
@@ -2,5 +2,4 @@ import '../../styles/front/app.scss';
 import 'bootstrap';
 import "@fortawesome/fontawesome-free/js/all.js";
 import "@fortawesome/fontawesome-free/css/all.css";
-
 console.log('hello webpack')

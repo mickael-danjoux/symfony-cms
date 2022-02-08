0f848bf4253ebb2f2ab9601bb633d36999612bff
Hugo-22 <hugo@ab6net.net>
2022-02-08 18:49:17 +0100

    migration symfony 5.4 => 6.0

--- src/Security/AdminAuthenticator.php
+++ src/Security/AdminAuthenticator.php
@@ -29,7 +29,7 @@ class AdminAuthenticator extends AbstractLoginFormAuthenticator
         $this->urlGenerator = $urlGenerator;
     }
 
-    public function authenticate(Request $request): PassportInterface
+    public function authenticate(Request $request): Passport
     {
         $email = $request->request->get('email', '');
 
